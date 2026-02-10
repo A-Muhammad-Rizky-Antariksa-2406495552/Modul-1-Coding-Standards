78a27c67993b2a0920eb16f15ad111d290981c0d
rizkyantariksa <rizkyantariksa02@gmail.com>
2026-02-10 20:41:47 +0700

    Add CreateProductFunctionalTest

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
@@ -0,0 +1,56 @@
+package id.ac.ui.cs.advprog.eshop.functional;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ExtendWith(SeleniumJupiter.class)
+class CreateProductFunctionalTest {
+
+    @LocalServerPort
+    private int serverPort;
+
+    @Value("${app.baseUrl:http://localhost}")
+    private String testBaseUrl;
+
+    private String baseUrl;
+
+    @BeforeEach
+    void setupTest() {
+        baseUrl = String.format("%s:%d", testBaseUrl, serverPort);
+    }
+
+    @Test
+    void createProduct_isCorrect(ChromeDriver driver) {
+        driver.get(baseUrl + "/product/create");
+
+        driver.findElement(By.id("nameInput")).sendKeys("Sampo Cap Bambang");
+        driver.findElement(By.id("quantityInput")).clear();
+        driver.findElement(By.id("quantityInput")).sendKeys("100");
+        driver.findElement(By.tagName("button")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.urlContains("/product/list"));
+        wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//table//tbody/tr[td[text()='Sampo Cap Bambang']]")
+        ));
+
+        String pageSource = driver.getPageSource();
+        assertTrue(pageSource.contains("Sampo Cap Bambang"));
+        assertTrue(pageSource.contains("100"));
+    }
+}
